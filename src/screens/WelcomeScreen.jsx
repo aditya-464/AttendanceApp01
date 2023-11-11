d1d7083619bddc1baa35da410086fd82644819af
Aditya Giri <adityagiri1911@gmail.com>
2023-11-11 11:56:34 +0530

    Theme file created

---
+++ src/screens/WelcomeScreen.jsx
@@ -0,0 +1,21 @@
+import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const WelcomeScreen = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <View>
+            <Image src='' alt='Image'></Image>
+        </View>
+        <View>
+
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/themes/Theme.jsx
@@ -0,0 +1,45 @@
+export const SPACING = {
+  space_2: 2,
+  space_4: 4,
+  space_8: 8,
+  space_10: 10,
+  space_12: 12,
+  space_15: 15,
+  space_16: 16,
+  space_18: 18,
+  space_20: 20,
+  space_24: 24,
+  space_28: 28,
+  space_30: 30,
+  space_32: 32,
+  space_36: 36,
+};
+
+export const FONTSIZE = {
+  size_8: 8,
+  size_10: 10,
+  size_12: 12,
+  size_14: 14,
+  size_16: 16,
+  size_18: 18,
+  size_20: 20,
+  size_24: 24,
+  size_28: 28,
+  size_30: 30,
+};
+
+export const BORDERRADIUS = {
+  radius_4: 4,
+  radius_8: 8,
+  radius_10: 10,
+  radius_15: 15,
+  radius_20: 20,
+  radius_25: 25,
+};
+
+export const COLORS = {
+  primaryDark: '#40513B',
+  secondaryDark: '#609966',
+  primaryLight: '#EDF1D6',
+  secondaryLight: '#9DC08B',
+};
