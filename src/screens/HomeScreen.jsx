2fe3e2e68e3fe9f400022a0306a6981e2b81b2a5
Aditya Giri <adityagiri1911@gmail.com>
2023-12-18 11:41:50 +0530

    Class list dummy data created

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,7 +1,52 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+  Dimensions,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import {COLORS, FONTFAMILY, FONTSIZE, SPACING} from '../themes/Theme';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const FlatListData = [
+  {
+    "id" : "item1",
+    "subject" : "Operating Systems",
+    "semester" : "5th",
+    "section" : "C"
+  },
+  {
+    "id" : "item2",
+    "subject" : "Artificial Intelligence",
+    "semester" : "5th",
+    "section" : "C"
+  },
+  {
+    "id" : "item3",
+    "subject" : "Computer Networks",
+    "semester" : "5th",
+    "section" : "C"
+  },
+  {
+    "id" : "item4",
+    "subject" : "Data Base Management System",
+    "semester" : "5th",
+    "section" : "C"
+  },
+  {
+    "id" : "item5",
+    "subject" : "Object Oriented Programming Language",
+    "semester" : "5th",
+    "section" : "C"
+  },
+];
+
+const FlatListItem = ({subject, semester, section}) => {
+  <View style={styles.ClassListItem}></View>
+};
 
 const HomeScreen = () => {
   return (
@@ -17,7 +62,19 @@ const HomeScreen = () => {
             color={COLORS.primaryDark}></Ionicons>
         </View>
       </View>
-      <View style={styles.MainContent}></View>
+      <View style={styles.MainContent}>
+        <View style={styles.CreateClass}>
+          <TouchableOpacity style={styles.CreateClassButton}>
+            <Ionicons
+              name="add"
+              size={FONTSIZE.size_30}
+              color={COLORS.primaryLight}></Ionicons>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.ClassList}>
+          <FlatList></FlatList>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -49,5 +106,21 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     alignItems: 'center',
     paddingBottom: SPACING.space_4,
+  },
+  CreateClass: {
+    position: 'absolute',
+    top: Dimensions.get('window').height - 200,
+    right: 20,
+  },
+  CreateClassButton: {
+    width: 70,
+    height: 70,
+    borderRadius: 100,
+    borderWidth: 2,
+    borderColor: COLORS.primaryLight,
+    backgroundColor: COLORS.secondaryDark,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
