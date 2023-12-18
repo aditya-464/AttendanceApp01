68fa4fd92ddfd7d5b9960c25f5e8bf1cf1b529c2
Aditya Giri <adityagiri1911@gmail.com>
2023-12-18 11:04:52 +0530

    Home screen created

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,14 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {COLORS, FONTFAMILY, FONTSIZE, SPACING} from '../themes/Theme';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <View style={styles.TitleBar}>
+        <View style={styles.Title}>
+          <Text style={styles.TitleText}>Home</Text>
+        </View>
+        <View style={styles.Menu}>
+          <Ionicons
+            name="list"
+            size={FONTSIZE.size_28}
+            color={COLORS.primaryDark}></Ionicons>
+        </View>
+      </View>
+      <View style={styles.MainContent}></View>
+    </SafeAreaView>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  TitleBar: {
+    width: '100%',
+    paddingHorizontal: SPACING.space_12,
+    paddingVertical: SPACING.space_4,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  Title: {
+    width: '80%',
+  },
+  TitleText: {
+    fontFamily: FONTFAMILY.poppins_bold,
+    fontSize: FONTSIZE.size_30,
+    color: COLORS.primaryDark,
+  },
+  Menu: {
+    width: '20%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingBottom: SPACING.space_4,
+  },
+});
